5ba654b37dd79419cfa1898f03faba18b2ff21c0
Loboo34 <rayovers40@gmail.com>
2026-02-25 18:03:26 +0300

    get activity

--- handlers/User/Activities.go
+++ handlers/User/Activities.go
@@ -8,7 +8,10 @@ import (
 	"github.com/Loboo34/travel/database"
 	model "github.com/Loboo34/travel/models"
 	"github.com/Loboo34/travel/utils"
+	"github.com/gorilla/mux"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 // user
@@ -40,6 +43,45 @@ func GetActivities(w http.ResponseWriter, r *http.Request) {
 	}
 
 	utils.RespondWithJson(w, http.StatusOK, "Fetched activities", activities)
+}
+
+func GetActivity(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		utils.RespondWithError(w, http.StatusMethodNotAllowed, "Only GET allowed")
+		return
+	}
+
+	vars := mux.Vars(r)
+	activityIDStr := vars["activityID"]
+	if activityIDStr == "" {
+		utils.RespondWithError(w, http.StatusBadRequest, "Missing activity ID")
+		return
+	}
+
+	activityID, err := primitive.ObjectIDFromHex(activityIDStr)
+	if err != nil {
+		utils.RespondWithError(w, http.StatusBadRequest, "Invalid activity ID")
+		return
+	}
+
+	activityCollection := database.DB.Collection("activities")
+	var activity model.Activity
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	err = activityCollection.FindOne(ctx, bson.M{"_id": activityID}).Decode(&activity)
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			utils.RespondWithError(w, http.StatusNotFound, "Activity not found")
+		} else {
+			utils.RespondWithError(w, http.StatusInternalServerError, "Error finding activity")
+			utils.Logger.Warn("Failed to fetch activity")
+		}
+		return
+	}
+
+	utils.RespondWithJson(w, http.StatusOK, "Fetched activity", activity)
 }
 
 //search activity
