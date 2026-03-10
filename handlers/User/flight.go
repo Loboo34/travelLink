c6036800408fb5e60032b839a599df14ace24f1f
Loboo34 <rayovers40@gmail.com>
2026-03-10 14:36:14 +0300

    flight search handler

--- handlers/User/flight.go
+++ handlers/User/flight.go
@@ -2,12 +2,17 @@ package user
 
 import (
 	"context"
+	"errors"
 	"net/http"
+	"net/url"
+	"strconv"
 	"time"
 
 	"github.com/Loboo34/travel/database"
 	model "github.com/Loboo34/travel/models"
+	"github.com/Loboo34/travel/service"
 	"github.com/Loboo34/travel/utils"
+
 	"github.com/gorilla/mux"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -82,39 +87,89 @@ func GetFlight(w http.ResponseWriter, r *http.Request) {
 
 }
 
+type FlightHandler struct {
+	flightService *service.FlightService
+}
+
+func NewFlightHandler(flightService *service.FlightService) *FlightHandler {
+	return &FlightHandler{flightService: flightService}
+}
+
 // search flights
-func SearchFlight(w http.ResponseWriter, r *http.Request) {
+func (h *FlightHandler) SearchFlight(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodGet {
 		utils.RespondWithError(w, http.StatusMethodNotAllowed, "Only GET allowed")
 		return
 	}
 
-	origin := r.URL.Query().Get("origin")
-	destination := r.URL.Query().Get("destinatiom")
-	date := r.URL.Query().Get("date")
-
-	search := bson.M{
-		"route.originAirportID":      origin,
-		"route.destinationAirportID": destination,
-		"departureTime": bson.M{
-			"$gte": date,
-		},
+	params, err := parseSearchParams(r.URL.Query())
+	if err != nil {
+		utils.RespondWithError(w, http.StatusBadRequest, "search params error")
+		return
 	}
 
-	filter := bson.M{
-		"flightID": bson.M{"$in": flightIDs},
-"isActive": true,
-"bookableSats": bson.M{"$gte": passengers},
-
+	result, err := h.flightService.Search(r.Context(), params)
+	if err != nil {
+		utils.RespondWithError(w, http.StatusBadRequest, "Error making search")
+		return
 	}
-	flightCollection := database.DB.Collection("flights")
-	var flight []model.Flight
 
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
+	utils.RespondWithJson(w, http.StatusOK, "", result)
+}
 
+func parseSearchParams(q url.Values) (model.FlightSearch, error) {
+	var params model.FlightSearch
 
-	_,err := flightCollection.Find(ctx, )
+	params.OriginCode = q.Get("originCode")
+	params.DestinationCode = q.Get("destinationCode")
 
+	depTime, err := time.Parse("", q.Get("depatureTime"))
+	if err != nil {
+		return params, errors.New("")
+	}
 
+	params.DepartureTime = depTime
+
+	if retStr := q.Get("returnDate"); retStr != "" {
+		retDate, err := time.Parse("", retStr)
+		if err != nil {
+
+			return params, errors.New("")
+		}
+		params.ReturnDate = &retDate
+	}
+
+	adults, err := strconv.Atoi(q.Get("adults"))
+	if err != nil || adults < 1 {
+		return params, errors.New("adults must be a number greater than 0")
+	}
+	params.Passengers.Adults = adults
+
+	if c := q.Get("children"); c != "" {
+		children, err := strconv.Atoi(c)
+		if err != nil {
+			return params, errors.New("children must be a number")
+		}
+		params.Passengers.Children = children
+	}
+
+	if i := q.Get("infants"); i != "" {
+		infants, err := strconv.Atoi(i)
+		if err != nil {
+			return params, errors.New("infants must be a number")
+		}
+		params.Passengers.Infant = infants
+	}
+
+	// optional fields with defaults handled in Validate()
+	params.CabinClass = model.CabinClassType(q.Get("cabinClass"))
+	params.SortBy = model.SortOptions(q.Get("sortBy"))
+
+	page, _ := strconv.Atoi(q.Get("page"))
+	params.Page = page
+
+	pageSize, _ := strconv.Atoi(q.Get("pageSize"))
+	params.PageSize = pageSize
+
+	return params, nil
 }
