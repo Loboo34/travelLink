8eb1e279f24e681729b7a8f6c3e06d513673d4c7
Loboo34 <rayovers40@gmail.com>
2026-03-20 20:05:20 +0300

    flight booking handler

--- handlers/booking.go
+++ handlers/booking.go
@@ -1,6 +1,59 @@
 package handlers
 
-//book flight
+import (
+	"encoding/json"
+	"net/http"
+
+	model "github.com/Loboo34/travel/models"
+	"github.com/Loboo34/travel/service"
+	"github.com/Loboo34/travel/utils"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type FlightBookingHandler struct {
+	bookingService *service.FlightBookingService
+}
+
+func NewFlightBookingHandler(bookingService *service.FlightBookingService) *FlightBookingHandler {
+	return &FlightBookingHandler{bookingService: bookingService}
+}
+
+// book flight
+func (h *FlightBookingHandler) FLightBooking(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		utils.RespondWithError(w, http.StatusMethodNotAllowed, "Only POST allowed")
+		return
+	}
+
+	userIDStr, err := utils.GetUserID()
+	if err != nil {
+		utils.RespondWithError(w, http.StatusBadRequest, "missing user ID")
+		return
+	}
+
+	userID, err := primitive.ObjectIDFromHex(userIDStr)
+	if err != nil {
+		utils.RespondWithError(w, http.StatusUnauthorized, "Invalid user ID")
+		return
+	}
+
+	var req model.FlightRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		utils.RespondWithError(w, http.StatusBadRequest, "Invalid request body")
+		return
+	}
+
+	defer r.Body.Close()
+
+	result, err := h.bookingService.Book(r.Context(), userID, req)
+	if err != nil {
+		utils.RespondWithError(w, http.StatusInternalServerError, "Error booking")
+		return
+	}
+
+	utils.RespondWithJson(w, http.StatusCreated, "Booking complete", result)
+}
+
 //book accommodation
 //book activities
 //book package
