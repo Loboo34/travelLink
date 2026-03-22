6b06ad9177c5c4749bdec8f02c6f1d1c967a52e5
Loboo34 <rayovers40@gmail.com>
2026-03-22 16:11:12 +0300

    activity booking handler

--- handlers/booking.go
+++ handlers/booking.go
@@ -54,25 +54,25 @@ func (h *FlightBookingHandler) FLightBooking(w http.ResponseWriter, r *http.Requ
 	utils.RespondWithJson(w, http.StatusCreated, "Booking complete", result)
 }
 
-//book accommodation
-type AccommodationBookingHandler struct{
+// book accommodation
+type AccommodationBookingHandler struct {
 	bookingService *service.AccommodationBookingService
 }
 
-func NewAccommodationBookingHandler(bookingService *service.AccommodationBookingService) *AccommodationBookingHandler{
+func NewAccommodationBookingHandler(bookingService *service.AccommodationBookingService) *AccommodationBookingHandler {
 	return &AccommodationBookingHandler{bookingService: bookingService}
 }
 
-func (h *AccommodationBookingHandler) AccommodationBooking(w http.ResponseWriter, r *http.Request){
-	if r.Method != http.MethodPost{
+func (h *AccommodationBookingHandler) AccommodationBooking(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
 		utils.RespondWithError(w, http.StatusMethodNotAllowed, "Only POST allowed")
-		return 
+		return
 	}
 
 	userIDStr, err := utils.GetUserID()
-	if err != nil{
+	if err != nil {
 		utils.RespondWithError(w, http.StatusBadRequest, "Missing user ID")
-		return 
+		return
 	}
 
 	userID, err := primitive.ObjectIDFromHex(userIDStr)
@@ -82,7 +82,7 @@ func (h *AccommodationBookingHandler) AccommodationBooking(w http.ResponseWriter
 	}
 
 	var req model.AccommodationBookingRequest
-	if err := json.NewDecoder(r.Body).Decode(&req); err != nil{
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
 		utils.RespondWithError(w, http.StatusBadRequest, "invalid request body")
 		return
 	}
@@ -90,14 +90,59 @@ func (h *AccommodationBookingHandler) AccommodationBooking(w http.ResponseWriter
 	defer r.Body.Close()
 
 	result, err := h.bookingService.Book(r.Context(), userID, req)
-	if err != nil{
+	if err != nil {
 		utils.RespondWithError(w, http.StatusInternalServerError, "error booking accommodations")
-		return 
+		return
 	}
 
 	utils.RespondWithJson(w, http.StatusCreated, "Booking complete", result)
 }
-//book activities
+
+// book activities
+type ActivityBookingHandler struct {
+	bookingService *service.ActivityBookingService
+}
+
+func NewActivityBookingHandler(bookingService *service.ActivityBookingService) *ActivityBookingHandler {
+	return &ActivityBookingHandler{bookingService: bookingService}
+}
+
+func (h *ActivityBookingHandler) ActivityBooking(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		utils.RespondWithError(w, http.StatusMethodNotAllowed, "Only POST allowed")
+		return
+	}
+
+	userIDStr, err := utils.GetUserID()
+	if err != nil {
+		utils.RespondWithError(w, http.StatusBadRequest, "Missing user ID")
+		return
+	}
+
+	userID, err := primitive.ObjectIDFromHex(userIDStr)
+	if err != nil {
+		utils.RespondWithError(w, http.StatusUnauthorized, "Invalid user ID")
+		return
+	}
+
+	var req model.ActivityBookingRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil{
+		utils.RespondWithError(w, http.StatusBadRequest, "Invalid request body")
+		return 
+	}
+
+	defer r.Body.Close()
+
+		result, err := h.bookingService.Book(r.Context(), userID, req)
+	if err != nil {
+		utils.RespondWithError(w, http.StatusInternalServerError, "error booking activity")
+		return
+	}
+
+	utils.RespondWithJson(w, http.StatusCreated, "Booking complete", result)
+
+}
+
 //book package
 //get users bookings
 //get booking by id
