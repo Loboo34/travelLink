e592b50da8b362e8b4010e7ac03cea25de3369d8
Loboo34 <rayovers40@gmail.com>
2026-03-31 22:43:36 +0300

    accommodation service/repo

---
+++ repository/accommodation._repo.go
@@ -0,0 +1,157 @@
+package repository
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"time"
+
+	model "github.com/Loboo34/travel/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type AccommodationRepo struct {
+	db *mongo.Database
+}
+
+func NewAccommodationRepo(db *mongo.Database) *AccommodationRepo {
+	return &AccommodationRepo{db: db}
+}
+
+func (r *AccommodationRepo) Add(ctx context.Context, accommodation *model.Accommodation) error {
+	_, err := r.db.Collection("accommodations").InsertOne(ctx, accommodation)
+	if err != nil {
+		return fmt.Errorf("adding accommodation: %w", err)
+	}
+
+	return nil
+}
+
+func (r *AccommodationRepo) Update(ctx context.Context, accommodationID primitive.ObjectID, name, description string, amenities, images []string) error {
+	var accom model.Accommodation
+	if err := r.db.Collection("accommodations").FindOne(ctx, bson.M{"_id": accommodationID}).Decode(&accom); err != nil {
+		return mongo.ErrNoDocuments
+	}
+
+	update := bson.M{
+		"$set": bson.M{
+			"name":        name,
+			"description": description,
+			"amenities":   amenities,
+			"images":      images,
+			"updatedAt":   time.Now(),
+		},
+	}
+
+	_, err := r.db.Collection("accommodations").UpdateOne(ctx, bson.M{"_id": accommodationID}, update)
+	if err != nil {
+		return fmt.Errorf("updating accommodation: %w", err)
+	}
+
+	return nil
+}
+
+func (r *AccommodationRepo) Delete(ctx context.Context, accommodationID primitive.ObjectID) error {
+	result, err := r.db.Collection("accommodations").DeleteOne(ctx, bson.M{"_id": accommodationID})
+	if err != nil {
+		return fmt.Errorf("deleting accommodation: %w", err)
+	}
+
+	if result.DeletedCount == 0 {
+		return fmt.Errorf("not found: %w", err)
+	}
+
+	return nil
+}
+
+func (r *AccommodationRepo) Availability(ctx context.Context, availability *model.AccommodationAvailability) error {
+	_, err := r.db.Collection("accommodation_availability").InsertOne(ctx, availability)
+	if err != nil {
+		return fmt.Errorf("creating availability: %w", err)
+	}
+
+	return nil
+}
+
+func (r *AccommodationRepo) IsActive(ctx context.Context, availabilityID primitive.ObjectID, isActive bool) error {
+
+	_, err := r.db.Collection("accommodation_availability").UpdateOne(ctx, bson.M{
+		"_id": availabilityID,
+	}, bson.M{
+		"$set": bson.M{
+			"isActive": isActive,
+		},
+	})
+	if err != nil {
+		return fmt.Errorf("updating status: %w", err)
+	}
+
+	return nil
+}
+
+func (r *AccommodationRepo) Remove(ctx context.Context, availabilityID primitive.ObjectID) error {
+	result, err := r.db.Collection("accommodation_availabiity").DeleteOne(ctx, bson.M{"_id": availabilityID})
+	if err != nil {
+		return fmt.Errorf("removing availability: %w", err)
+	}
+
+	if result.DeletedCount == 0 {
+		return fmt.Errorf("not found: %w", err)
+	}
+
+	return nil
+}
+
+func (r *AccommodationRepo) GetAvailabilities(ctx context.Context) (*model.AccommodationAvailability, error) {
+	_, err := r.db.Collection("accommodation_Availability").Find(ctx, bson.M{})
+	if err != nil {
+		if errors.Is(err, mongo.ErrNoDocuments) {
+			return nil, fmt.Errorf("accommodation not available: %w", err)
+		}
+		return nil, fmt.Errorf("getting available accommodations")
+
+	}
+
+	return &model.AccommodationAvailability{}, nil
+}
+
+func (r *AccommodationRepo) GetAvailability(ctx context.Context, availabilityID primitive.ObjectID) (*model.AccommodationAvailability, error) {
+	_, err := r.db.Collection("accommodation_Availability").Find(ctx, bson.M{"_id": availabilityID})
+	if err != nil {
+		if errors.Is(err, mongo.ErrNoDocuments) {
+			return nil, fmt.Errorf("accommodation not available: %w", err)
+		}
+		return nil, fmt.Errorf("getting available accommodations")
+
+	}
+
+	return &model.AccommodationAvailability{}, nil
+}
+
+func (r *AccommodationRepo) GetAccomodation(ctx context.Context, accommodationID primitive.ObjectID) (*model.Accommodation, error) {
+	_, err := r.db.Collection("accommodations").Find(ctx, bson.M{"_id": accommodationID})
+	if err != nil {
+		if errors.Is(err, mongo.ErrNoDocuments) {
+			return nil, fmt.Errorf("accommodation not available: %w", err)
+		}
+		return nil, fmt.Errorf("getting  accommodations")
+
+	}
+
+	return &model.Accommodation{}, nil
+}
+
+func (r *AccommodationRepo) GetAccomodationS(ctx context.Context) (*model.Accommodation, error) {
+	_, err := r.db.Collection("accommodations").Find(ctx, bson.M{})
+	if err != nil {
+		if errors.Is(err, mongo.ErrNoDocuments) {
+			return nil, fmt.Errorf("accommodation not available: %w", err)
+		}
+		return nil, fmt.Errorf("getting  accommodations")
+
+	}
+
+	return &model.Accommodation{}, nil
+}
---
+++ service/accommodation._service.go
@@ -0,0 +1,157 @@
+package service
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	model "github.com/Loboo34/travel/models"
+	"github.com/Loboo34/travel/repository"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type AccommodationService struct {
+	accommodationRepo *repository.AccommodationRepo
+}
+
+func NewAccommodationService(accommodationRepo *repository.AccommodationRepo) *AccommodationService {
+	return &AccommodationService{accommodationRepo: accommodationRepo}
+}
+
+type AccommodationRequest struct {
+	HostID       *primitive.ObjectID `json:"hostID"`
+	PropertyType model.PropertyType  `json:"propertyType"`
+	Name         string              `json:"name"`
+	Address      model.Address       `json:"address"`
+	Amenities    []model.Amenity     `json:"amenities"`
+	Description  string              `json:"description"`
+	Images       []string            `json:"images"`
+	Location     model.GeoLocation   `json:"location"`
+	RoomType     []model.RoomType    `json:"roomType"`
+}
+
+type AccommodationResult struct {
+	AccommodationID primitive.ObjectID `json:"accommodationID"`
+	PropertyType    model.PropertyType `json:"propertyType"`
+	Name            string             `json:"name"`
+	Adress          model.Address      `json:"address"`
+}
+
+type AccommodationUpdate struct {
+	Name        string   `json:"name"`
+	Description string   `json:"description"`
+	Amenities   []string `json:"amenities"`
+	Images      []string `json:"images"`
+}
+
+type AvailabilityRequest struct {
+	AccommodationID primitive.ObjectID `json:"accommodationID"`
+	RoomTypeID      model.RoomType     `json:"roomTypeID"`
+	Date            time.Time          `json:"date"`
+	TotalRooms      int                `json:"totalRooms"`
+	PricePerNight   int64              `json:"pricePerNight"`
+}
+
+type AvailabilityResult struct {
+	AccommodationID primitive.ObjectID `json:"accommodationID"`
+	RoomTypeID      model.RoomType     `json:"roomTypeID"`
+	TotalRooms      int                `json:"totalRooms"`
+}
+
+func (s *AccommodationService) Add(ctx context.Context, req AccommodationRequest) (*AccommodationResult, error) {
+
+	accommodationID := primitive.NewObjectID()
+
+	accommodation := &model.Accommodation{
+		ID:           accommodationID,
+		HostID:       req.HostID,
+		PropertyType: req.PropertyType,
+		Name:         req.Name,
+		Address:      req.Address,
+		Description:  req.Description,
+		Amenities:    req.Amenities,
+		Images:       req.Images,
+		Location:     req.Location,
+		RoomType:     req.RoomType,
+		Rating:       0,
+		ReviewCount:  0,
+		CreatedAt:    time.Now(),
+		UpdatedAt:    time.Now(),
+	}
+
+	if err := s.accommodationRepo.Add(ctx, accommodation); err != nil {
+		return nil, fmt.Errorf("acceing accommodation: %w", err)
+	}
+
+	return &AccommodationResult{
+		AccommodationID: accommodationID,
+		PropertyType:    req.PropertyType,
+		Name:            req.Name,
+		Adress:          req.Address,
+	}, nil
+
+}
+
+func (s *AccommodationService) Update(ctx context.Context, accommodationId primitive.ObjectID, req AccommodationUpdate) (*AccommodationResult, error) {
+	if err := s.accommodationRepo.Update(ctx, accommodationId, req.Name, req.Description, req.Amenities, req.Images); err != nil {
+		return nil, fmt.Errorf("updating accommodtion: %w ", err)
+	}
+
+	return &AccommodationResult{
+
+		AccommodationID: accommodationId,
+		Name:            req.Name,
+	}, nil
+}
+
+func (s *AccommodationService) Delete(ctx context.Context, accommodationID primitive.ObjectID) error {
+	if err := s.accommodationRepo.Delete(ctx, accommodationID); err != nil {
+		return fmt.Errorf("deleting accommodation: %w", err)
+	}
+
+	return nil
+}
+
+func (s *AccommodationService) Availability(ctx context.Context, req AvailabilityRequest) (*AvailabilityResult, error) {
+
+	availabilityID := primitive.NewObjectID()
+	availability := &model.AccommodationAvailability{
+		ID:              availabilityID,
+		AccommodationID: req.AccommodationID,
+		RoomTypeID:      req.RoomTypeID.ID,
+		Date:            req.Date,
+		TotalRooms:      req.TotalRooms,
+		ReservedRooms:   0,
+		PricePerNight:   req.PricePerNight,
+		IsActive:        true,
+		CreatedAt:       time.Now(),
+		UpdatedAt:       time.Now(),
+	}
+
+	if err := s.accommodationRepo.Availability(ctx, availability); err != nil {
+		return nil, fmt.Errorf("creating availability: %w", err)
+	}
+
+	return &AvailabilityResult{
+		AccommodationID: req.AccommodationID,
+		RoomTypeID:      req.RoomTypeID,
+		TotalRooms:      req.TotalRooms,
+	}, nil
+}
+
+func (s *AccommodationService) IsActive(ctx context.Context, availabilityID primitive.ObjectID, status bool) error {
+	if err := s.accommodationRepo.IsActive(ctx, availabilityID, status); err != nil {
+		return fmt.Errorf("updating status: %w", err)
+	}
+
+	return nil
+
+}
+
+func (s *AccommodationService) Remove(ctx context.Context, accommodationID primitive.ObjectID) error {
+	if err := s.accommodationRepo.Remove(ctx, accommodationID); err != nil {
+		return fmt.Errorf("deleting accommodation: %w", err)
+	}
+
+	return nil
+}
